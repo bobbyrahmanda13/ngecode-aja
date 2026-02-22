7f82aed9f931e2361bbeb4cddc2913179df2ba95
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2026-02-23 06:54:19 +0700

    update git push

--- golang/gitCommand/main.go
+++ golang/gitCommand/main.go
@@ -11,8 +11,8 @@ import (
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 
-	// 1. Meminta input pesan (seperti read -p)
 	fmt.Print("Git Message : ")
+	// 1. Meminta input pesan (seperti read -p)
 	gitMessage, _ := reader.ReadString('\n')
 	gitMessage = strings.TrimSpace(gitMessage)
 
@@ -26,13 +26,22 @@ func main() {
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
-	// 4. Menjalankan git push
-	exec.Command("git", "push").Run()
-
 	err := cmd.Run()
 	if err != nil {
 		fmt.Printf("Gagal commit: %v\n", err)
 	} else {
 		fmt.Println("Berhasil commit!")
+	}
+
+	fmt.Println("Sedang melakukan push...")
+	pushCmd := exec.Command("git", "push")
+	pushCmd.Stdout = os.Stdout
+	pushCmd.Stderr = os.Stderr
+
+	errPush := pushCmd.Run()
+	if errPush != nil {
+		fmt.Printf("Gagal push: %v\n", errPush)
+	} else {
+		fmt.Println("Berhasil push ke remote!")
 	}
 }
